eca85f7b010de656b93b471642065f17e094faaf
Nhlakanipho <nhalakaniphoradebe337@gmail.com>
2024-10-14 09:23:57 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+// Port Defined
+const PORT = 3000;
+
+// root response, with a Hello response
+app.get("/",(req,res)=>{
+    res.send('Hello');
+});
+
+// another response route
+app.get('/greet', (req, res) => {
+    res.json({ message: 'Hello from the API!' });
+  });
+
+
+// Server listening on port
+app.listen(PORT,()=>{
+    console.log(`Server running on http://localhost: ${PORT}`);
+})
+
+
+
